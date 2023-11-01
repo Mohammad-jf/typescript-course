072558c8225866a17c01fbb70b28002a999d525d
mohammad jalifathi <m.h.jalifathi@gmail.com>
2023-11-02 03:08:07 +0330

    lesson4

--- lesson4/build/js/main.js
+++ lesson4/build/js/main.js
@@ -3,6 +3,9 @@
 let myName;
 let userName;
 userName = 'mohammad ';
+// interface mathFunction {
+//   (a: number, b: number): number;
+// }
 const add = (a, b) => {
     return a + b;
 };
@@ -15,3 +18,21 @@ let subtract = function (a, b) {
     return a - b;
 };
 logMessage(subtract(10, 6));
+// optional parameters
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+};
+const addAllDefault = (a, b, c = 2) => {
+    return a + b + c;
+};
+logMessage(addAll(10, 20, 40));
+// with default values
+logMessage(addAllDefault(10, 20));
+// rest parameters
+const total = (...nums) => {
+    return nums.reduce((prev, current) => prev + current, 0);
+};
+logMessage(total(1, 2, 3, 4, 5));
--- lesson4/src/main.ts
+++ lesson4/src/main.ts
@@ -19,6 +19,10 @@ userName = 'mohammad ';
 
 type mathFunction = (a: number, b: number) => number;
 
+// interface mathFunction {
+//   (a: number, b: number): number;
+// }
+
 const add: mathFunction = (a, b) => {
   return a + b;
 };
@@ -35,3 +39,27 @@ let subtract: mathFunction = function (a, b) {
 };
 
 logMessage(subtract(10, 6));
+
+// optional parameters
+
+const addAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== 'undefined') {
+    return a + b + c;
+  }
+
+  return a + b;
+};
+const addAllDefault = (a: number, b: number, c: number = 2): number => {
+  return a + b + c;
+};
+
+logMessage(addAll(10, 20, 40));
+// with default values
+logMessage(addAllDefault(10, 20));
+
+// rest parameters
+const total = (...nums: number[]): number => {
+  return nums.reduce((prev, current) => prev + current, 0);
+};
+
+logMessage(total(1, 2, 3, 4, 5));
