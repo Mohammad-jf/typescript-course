05c57aaa7e82ebcaa26eb933b1d8ce27808b235f
mohammad jalifathi <m.h.jalifathi@gmail.com>
2023-11-06 23:50:09 +0330

    lesson 7

--- lesson7/build/js/main.js
+++ lesson7/build/js/main.js
@@ -1,1 +1,8 @@
 "use strict";
+const todaysTransactions = {
+    pizza: -5,
+    book: -10,
+    income: 55,
+};
+const prop = 'pizza';
+console.log(todaysTransactions[prop]);
--- lesson7/src/main.ts
+++ lesson7/src/main.ts
@@ -1,3 +1,15 @@
 interface TransactionObj {
   [index: string]: number;
 }
+
+const todaysTransactions: TransactionObj = {
+  pizza: -5,
+  book: -10,
+  income: 55,
+};
+
+const prop: string = 'pizza';
+
+
+
+console.log(todaysTransactions[prop]);
