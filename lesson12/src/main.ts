2e29591d75c73415e5f468a7b09bfe803f9009f0
mohammad jalifathi <m.h.jalifathi@gmail.com>
2023-12-20 00:12:59 +0330

    practice

--- lesson12/src/main.ts
+++ lesson12/src/main.ts
@@ -80,3 +80,21 @@ type Musician = {
 
 // tuples
 let myTupel: [string, number, boolean] = ['mohammad', 22, true];
+
+// generics
+function arrayToObject<T>(array: [string, T][]) {
+  const obj: {
+    [index: string]: T;
+  } = {};
+  array.forEach(([key, value]) => {
+    obj[key] = value;
+  });
+
+  return obj;
+}
+
+const arr: [string, number | boolean][] = [
+  ['keyOne', 1],
+  ['keyTwo', 2],
+  ['keyThree', true],
+];
